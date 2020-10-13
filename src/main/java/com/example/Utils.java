edac83b62e73fe5a17d1f506cfd299ba8c23ab1e
Ilnar Khafizov <ilnar_khafizov@epam.com>
2020-10-13 10:45:49 +0400

    Initial commit

---
+++ src/main/java/com/example/Utils.java
@@ -0,0 +1,12 @@
+package com.example;
+
+public class Utils {
+
+    public String concatenateWords(String first, String second) {
+        return ""; //todo
+    }
+
+    public int computeFactorial(int number) {
+        return 0; //todo
+    }
+}
